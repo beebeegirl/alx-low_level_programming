530fcfe40a593c9b323bf1961c9fd54045986794
beebeegirl <beegirlnite@gmail.com>
2023-05-26 14:00:56 -0700

    C - Function pointers solved

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ * @name: the person's name
+ * @f: a pointer to the function
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+
+	f(name);
+}
