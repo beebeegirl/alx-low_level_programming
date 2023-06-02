48111f675d926089318740bf4e79833804f1d3d0
beebeegirl <beegirlnite@gmail.com>
2023-06-02 07:46:41 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t n = 0;
+size_t s = 0;
 
 while (h)
 {
@@ -21,8 +21,8 @@ printf("[0] (nil)\n");
 else
 printf("[%u] %s\n", h->len, h->str);
 h = h->next;
-n++;
+s++;
 }
 
-return (n);
+return (s);
 }
