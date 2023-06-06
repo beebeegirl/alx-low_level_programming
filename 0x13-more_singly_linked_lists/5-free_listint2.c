a425efee26bb5d4db641b735f0f55f8670298f61
beebeegirl <beegirlnite@gmail.com>
2023-06-06 05:17:33 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,7 @@
 
 /**
  * free_listint2 - function that frees a listint_t list.
- * @head:pointer to the listint_t list to be freed
+ * @head: pointer to the listint_t list to be freed
  *
  * Return: void
  */
@@ -21,5 +21,5 @@ void free_listint2(listint_t **head)
 		*head = next;
 	}
 		*head = NULL;
-	
+
 }
