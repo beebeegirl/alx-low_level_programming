a8acecdbce142b76be88552d3c482eddb6244531
Blessing Nite <129007996+beebeegirl@users.noreply.github.com>
2023-06-11 21:59:19 +0100

    Delete 0x09-static_libraries directory

--- 0x09-static_libraries/1-memcpy.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	return (dest);
-}
--- 0x09-static_libraries/2-strchr.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "main.h"
-#include <stdlib.h>
-#include <string.h>
-
-char *_strchr(char *s, char c)
-{
-	return (s);
-}
